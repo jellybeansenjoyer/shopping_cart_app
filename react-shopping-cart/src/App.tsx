8072ce9965257cd2346f07488846012e5736490d
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2023-12-25 18:54:14 +0530

    getting datat from the api

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -1,16 +1,32 @@
-import {useState} from 'react';
-import {useQuery} from 'react-query';
+import { useState } from "react";
+import { useQuery } from "react-query";
 //Components
-import Drawer from '@material-ui/core/Drawer';
-import { LinearProgress } from '@material-ui/core';
-import Grid from '@material-ui/core';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import Badge from '@material-ui/core';
-
+import Drawer from "@material-ui/core/Drawer";
+import { LinearProgress } from "@material-ui/core";
+import Grid from "@material-ui/core";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core";
+import { Wrapper } from "./App.styles";
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
 const App = () => {
-  return (
-    <div className="App">Start</div>
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
   );
-}
+  console.log(data);
+  return <div className="App">Start</div>;
+};
 
 export default App;
